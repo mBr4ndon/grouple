5229111eae8d5820c87778b8b9ed0cfa99e7f6da
Márcio Brandão <brandao.marcio.xix@gmail.com>
2024-10-23 20:26:08 +0100

    feat: created group about page

---
+++ src/app/group/about/[groupId]/page.tsx
@@ -0,0 +1,14 @@
+
+interface Props {
+    params: {
+        groupId: string
+    }
+}
+
+const GroupAboutPage = (props: Props) => {
+  return (
+    <div>GroupAboutPage</div>
+  )
+}
+
+export default GroupAboutPage
